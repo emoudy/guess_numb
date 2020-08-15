f8cc35a7cd23c828234cb995986df3ef88446fa5
Elda Moudy <elda@moudy.net>
2020-08-15 13:35:03 -0700

    displaying images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import block0 from './images/block0.JPG';
+import block1 from './images/block1.JPG';
+import block2 from './images/block2.JPG';
+import block3 from './images/block3.JPG';
+import block4 from './images/block4.JPG';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <ul className="BlockImages">
+          <li><img src={block0} alt="block0" /></li>
+          <li><img src={block1} alt="block1" /></li>
+          <li><img src={block2} alt="block2" /></li>
+          <li><img src={block3} alt="block3" /></li>
+          <li><img src={block4} alt="block4" /></li>
+        </ul>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
